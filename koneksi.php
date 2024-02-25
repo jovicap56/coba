51b88eba463a6a9508305513f6570697c80c37c0
jovicap56 <159878203+jovicap56@users.noreply.github.com>
2024-02-25 08:51:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","inventaris_lab") or die ("koneksi gagal".mysqli_connect_error());
+$koneksi = mysqli_connect("localhost","root","","jovica") or die ("koneksi gagal".mysqli_connect_error());
 ?>
